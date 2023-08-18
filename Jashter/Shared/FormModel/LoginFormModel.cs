00e5b7cb5f0473d8cbafdabcef1c5c0ec7fa10bb
Fuki Sato <lego.sato.4135@gmail.com>
2023-08-18 16:12:43 +0900

    implement login function

---
+++ Jashter/Shared/FormModel/LoginFormModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace Jashter.Shared.FormModel
+{
+	public class LoginFormModel
+	{
+		[Required]
+		public string Username { get; set; } = null!;
+
+		[Required]
+		public string Password { get; set; } = null!;
+	}
+}
+
